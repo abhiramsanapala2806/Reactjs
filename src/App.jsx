a1126e60ec9e52299eb7f41a3acaf9510eb8770a
abhiramsanapala2806 <abhiramsanapala2806@Gmail.com>
2026-09-08 10:26:06 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -12,3 +14,4 @@ function App() {
 }
 
 export default App
+ 
